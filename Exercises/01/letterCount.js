8fc150f7bbf473d5199296ca4200362c015786c4
Ulises Gonzalez <c_ulii@hotmail.com>
2019-11-26 07:07:54 -0600

    Update letterCount.js

--- Exercises/01/letterCount.js
+++ Exercises/01/letterCount.js
@@ -1,7 +1,25 @@
 //letterCount
 
-const count = function(word = ""){
+const count = function(word = "", skip = ''){
+  let n = 0;
+  let l = '';
+
   if(word){
+    let arr = word.split("");
     
+    arr.filter( q => q !== l );
+
+    while(arr.length > 0){
+      [l] = arr;
+      n++;
+  
+      arr.filter( q => q !== l );
+    }
   }
+  return n;
 }
+
+
+count("banana and kebab", 'b');
+
+//expected output: 5
