7f99b49ff4ee9885beb872b9caec30ae93e6cd9c
Ulises Gonzalez <c_ulii@hotmail.com>
2019-11-27 14:11:08 -0600

    Update swapi.js

--- Excercises/Day03/swapi.js
+++ Excercises/Day03/swapi.js
@@ -1,17 +1,19 @@
 var App = function(){
    console.log('inside App');
 }
+
 App.prototype.getallPeople = async function(){
 
  //consume aqui el servicio https://swapi.co/api/people/ 
  //y regresa un array de todos los nombres de las personas que tengan una altura mayor a 120
- 
+
  const url = "https://swapi.co/api/people/"
  const response = await fetch(url);
  const people = await response.json();
- return people.filter(p => p.height > 120);
+ console.log(people.results);
+ return people.results.filter(p => p.height > 120);
 };
 
 const app = new App();
 console.log('names')
-app.getallPeople(people => console.log(people));
+app.getallPeople().then(data => console.log(data));
