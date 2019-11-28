5bfe8ca2cff3cddea986a4d892d91fe3d5674caf
Ulises Gonzalez <c_ulii@hotmail.com>
2019-11-28 16:39:16 -0600

    Update primeNumbers.js
    Solved sort numerically

--- Excercises/Day02/primeNumbers.js
+++ Excercises/Day02/primeNumbers.js
@@ -2,13 +2,10 @@
 
 const prime = (arr) => {
   return arr.filter(a => {
-    for(let i = 2; i < a; i++){
-      if(i % a === 0){
-        return true;
-      }
-    }
-    return false;
-  }).sort();
+    let i = 2
+    while((a % i) !== 0 && i++ < a);
+    return i===a
+  }).sort((a, b) => a - b );
 }
 
 console.log(prime([2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]));
